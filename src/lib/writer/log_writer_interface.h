5cdde99001cb5ae855147600892a399d3368dd8b
jodyvole <jodyvole@student.21-school.ru>
2025-08-15 17:34:27 +0300

    add writer interface

---
+++ src/lib/writer/log_writer_interface.h
@@ -0,0 +1,17 @@
+#ifndef LOGGER_SRC_LIB_WRITER_LOG_WRITER_INTERFACE_H_
+#define LOGGER_SRC_LIB_WRITER_LOG_WRITER_INTERFACE_H_
+
+#include <string>
+
+namespace lib {
+
+class LogWriterInterface {
+ public:
+  virtual ~LogWriterInterface() = default;
+
+  virtual void Write(const std::string& log) = 0;
+};
+
+} // namespace lib
+
+#endif // LOGGER_SRC_LIB_WRITER_LOG_WRITER_INTERFACE_H_
