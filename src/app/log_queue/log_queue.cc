9ef6a3fc4b6e8763d2203d5b90ce5f1efc901782
jodyvole <jodyvole@student.21-school.ru>
2025-08-19 17:00:19 +0300

    implement LogQueue

---
+++ src/app/log_queue/log_queue.cc
@@ -0,0 +1,34 @@
+#include "log_queue.h"
+
+namespace app {
+
+void LogQueue::Push(const LogMessage& msg) {
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(msg);
+  }
+  cond_.notify_one();
+}
+
+bool LogQueue::Pop(LogMessage& msg) {
+  std::unique_lock<std::mutex> lock(mutex_);
+  cond_.wait(lock, [&]{ return !queue_.empty() || is_stop_; });
+  
+  if (is_stop_ && queue_.empty()) {
+    return false;
+  }
+
+  msg = queue_.front();
+  queue_.pop();
+  return true;
+}
+
+void LogQueue::Stop() {
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    is_stop_ = true;
+  }
+  cond_.notify_all();
+}
+
+} // namespace app
---
+++ src/app/log_queue/log_queue.h
@@ -0,0 +1,27 @@
+#ifndef LOGGER_SRC_APP_LOG_QUEUE_LOG_QUEUE_H_
+#define LOGGER_SRC_APP_LOG_QUEUE_LOG_QUEUE_H_
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+#include "../message/log_message.h"
+
+namespace app {
+
+class LogQueue {
+ public:
+  void Push(const LogMessage& msg);
+  bool Pop(LogMessage& msg);
+  void Stop();
+
+ private:
+  std::queue<LogMessage> queue_;
+  std::mutex mutex_;
+  std::condition_variable cond_;
+  bool is_stop_ = false;
+};
+
+} // namespace app
+
+#endif // LOGGER_SRC_APP_LOG_QUEUE_LOG_QUEUE_H_
